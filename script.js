c5b11768f8a8faf86fcbcec43c9f3c904e03c28b
Nathan Polomark <polomarkn@hotmail.com>
2020-09-27 21:11:11 -0700

    Added timer and saving score

--- script.js
+++ script.js
@@ -7,30 +7,37 @@ const question4 = document.querySelector("#question4")
 const question5 = document.querySelector("#question5")
 const questions = [question1, question2, question3, question4, question5]
 const results = document.querySelector("#results")
-const yourScore = document.querySelector("#score")
-const highScore = document.querySelector("#high-score")
+const timer = document.querySelector("#timer")
 
+var doneQuiz = false
 var score = 0
-function startCountdown(seconds) {
-    var timeLeft = 60;
-    
-    var timeInterval = setInterval(function() {
-        timeLeft--
-        if (timeLeft = 0) {
-            clearInterval(interval)
+var timeLeft = 60
+var timeInterval = setInterval(startCountdown, 1000)
 
-            questions.classList.add("hide")
-            results.classList.remove("hide")
-        }
-    }, 1000);
+function startCountdown() {
+    timer.textContent = timeLeft
+    timeLeft--
+
+    if (timeLeft <= 0) {
+        clearInterval(timeInterval)
+        questions[0, 1, 2, 3, 4].classList.add("hide")
+        console.log(questions)
+        results.classList.remove("hide")
+        timer.classList.add("hide")
+    }
+
+    if (doneQuiz) {
+        clearInterval(timeInterval)
+        timer.classList.add("hide")
+    }
 }
+
 startButton.addEventListener("click", startQuiz)
 
 
 function startQuiz() {
     score = 0
-    startCountdown(60)
-    // console.log(timeLeft)
+    startCountdown()
     startScreen.classList.add("hide")
     question1.classList.remove("hide")
 }
@@ -38,11 +45,13 @@ function startQuiz() {
 question1.addEventListener("click", checkAnswer1)
 
 function checkAnswer1(event) {
-    if ( event.target.classList.contains("correct"))
-    score++
-    console.log("you are correct")
-    askQuestion2()
-
+    if ( event.target.classList.contains("correct")) {
+        score++
+        askQuestion2()
+    } else {
+        timeLeft = (timeLeft - 5)
+        askQuestion2()
+    }
 }
 
 function askQuestion2() {
@@ -53,9 +62,13 @@ function askQuestion2() {
 question2.addEventListener("click", checkAnswer2)
 
 function checkAnswer2(event) {
-    if ( event.target.classList.contains("correct"))
-    score++
-    askQuestion3()
+    if ( event.target.classList.contains("correct")) {
+        score++
+        askQuestion3()
+    } else {
+        timeLeft = (timeLeft - 5)
+        askQuestion3()
+    }
 }
 
 function askQuestion3() {
@@ -66,9 +79,13 @@ function askQuestion3() {
 question3.addEventListener("click", checkAnswer3)
 
 function checkAnswer3(event) {
-    if ( event.target.classList.contains("correct"))
-    score++
-    askQuestion4()
+    if ( event.target.classList.contains("correct")) {
+        score++
+        askQuestion4()
+    } else {
+        timeLeft = (timeLeft - 5)
+        askQuestion4()
+    }
     console.log(score)
     console.log("ask question 4")
 }
@@ -82,9 +99,13 @@ function askQuestion4() {
 question4.addEventListener("click", checkAnswer4)
 
 function checkAnswer4(event) {
-    if ( event.target.classList.contains("correct")) 
-    score++
-    askQuestion5()
+    if ( event.target.classList.contains("correct")) {
+        score++
+        askQuestion5()
+    } else {
+        timeLeft = (timeLeft - 5)
+        askQuestion5()
+    }
 }
 function askQuestion5() {
     question4.classList.add("hide")
@@ -93,7 +114,7 @@ function askQuestion5() {
 
 question5.addEventListener("click", checkAnswer5)
 function checkAnswer5(event) {
-    if ( event.target.classList.contains("correct"))
+    if ( event.target.classList.contains("correct")) 
     score++
     console.log("your score is " + score)
     showFinalPage()
@@ -102,4 +123,19 @@ function checkAnswer5(event) {
 function showFinalPage () {
     question5.classList.add("hide")
     results.classList.remove("hide")
+    doneQuiz = true
+}
+
+document.querySelector(".submit").addEventListener("click", submitScore)
+
+function submitScore() {
+    var userName = document.querySelector(".form-control").value
+
+    localStorage.setItem("username", userName)
+    localStorage.setItem("score", score)
+
+    var inputEmail = localStorage.getItem("username")
+    var inputScore = localStorage.getItem("score")
+    document.querySelector(".input-username").textContent = inputEmail
+    document.querySelector(".input-score").textContent = inputScore
 }
